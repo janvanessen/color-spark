847e8441cf2eb4b7085a4a1ae68e43d0c572a52c
janvanessen <jan.vanessen@babelo.de>
2023-10-12 18:57:08 +0200

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,61 @@
+<template>
+
+<section class="section">
+  <div class="container">
+    <h2 class="title">Hello Counter</h2>
+    <div class="level">
+      <div class="level-item">
+      <h1>
+        <button class="button" @click="incrementCounter">
+        +
+        </button>
+        <span>{{counter}}</span>
+        <button class="button" @click="decrementCounter">
+        -
+        </button>
+        </h1>
+      </div>
+
+    </div>
+    <button class="button" @click="getColors">
+     getColors
+    </button>
+  </div>
+</section>
+
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  mounted() {
+    console.log('Component mounted.')
+  },
+  data() {
+    return {
+      title: "Counter",
+      counter: 0
+    };
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++;
+    },
+    decrementCounter() {
+      this.counter--;
+    },
+    async getColors() {
+      console.log('call colors');
+      const { data } = await axios.get("/api/colors", 
+        { 
+          params: {
+            text: "hello world"
+          }
+        });
+      console.log(data);
+    }
+  }
+};
+</script>
+
