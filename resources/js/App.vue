2fd69d14fe3ada950feccbbfdca19a821c15a377
janvanessen <jan.vanessen@babelo.de>
2023-10-14 09:37:39 +0200

    Add PrimeVue UI

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,47 +1,88 @@
 <template>
-
-<section class="section">
-  <div class="container">
-    <h2 class="title">Color Spark</h2>
-    <input v-model="input" placeholder="edit me" v-on:keyup.enter="getColors" />
-    <button class="button" @click="getColors">
-     getColors
-    </button>
-    <div>
-    {{output}}
+    <div class="main-container">
+        <h1>Color Spark</h1>
+        <div class="tagline">Create unique color schemes from text descriptions</div>        
+        <div class="form-wrapper">
+            <InputText
+                class="form-input"
+                v-model="value"
+                autofocus
+                v-on:keyup.enter="getColors"
+            />
+            <Button
+                class="form-button p-button-md"
+                label="Create"
+                @click="getColors"
+            />
+        </div>
+        <div class="color-sets">
+            {{ output }}
+        </div>
     </div>
-  </div>
-</section>
-
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-  mounted() {
-    console.log('Component mounted.')
-  },
-  data() {
-    return {
-      input: '',
-      output: '',
-    };
-  },
-  methods: {
-    async getColors() {
-      console.log('call colors');
-      this.output = '';
-      const { data } = await axios.get("/api/colors", 
-        { 
-          params: {
-            text: this.input
-          }
-        }); 
-      console.log(data);
-      this.output = data.answer;
-    }
-  }
+    mounted() {
+        console.log("Component mounted.");
+    },
+    data() {
+        return {
+            input: "",
+            output: "",
+        };
+    },
+    methods: {
+        async getColors() {
+            console.log("call colors");
+            this.output = "";
+            const { data } = await axios.get("/api/colors", {
+                params: {
+                    text: this.input,
+                },
+            });
+            console.log(data);
+            this.output = data.answer;
+        },
+    },
 };
 </script>
 
+<style scoped>
+.main-container {
+    margin: 80px auto;
+    text-align: center;
+}
+
+.form-wrapper {
+    width: 600px;
+    margin: 0px auto;
+}
+
+.form-input {
+    width: 400px;
+}
+
+.form-button {
+    margin-left: 10px;
+    width: 100px;
+}
+
+.color-sets {
+    width: 600px;
+    margin-top: 100px;
+}
+
+h1 {
+  font-size: 50px;
+  line-height: 10px;
+}
+
+.tagline {
+  font-size: 18px;
+  margin-bottom: 30px;
+}
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,12 @@
-// import './bootstrap';
+import {createApp} from 'vue';
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
+import PrimeVue from 'primevue/config';
+import Button from "primevue/button";
+import InputText from 'primevue/inputtext';
+import App from './App.vue';
 
-
-import {createApp} from 'vue'
-
-import App from './App.vue'
-
-createApp(App).mount("#app")
+const app = createApp(App);
+app.use(PrimeVue);
+app.component('Button', Button);
+app.component('InputText', InputText);
+app.mount("#app");
